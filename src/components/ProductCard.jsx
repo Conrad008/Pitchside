0300bf4fa9afe41971c558acda9c6724ad47b3c7
Conrad008 <kipngeno7180@gmail.com>
2026-07-05 00:40:22 +0300

    feat:add handleAddToCart logic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,5 +2,22 @@ import { useContext } from 'react';
 import { ShoppingBag } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { CartContext } from '../context/cartContext';
+import { CartContext } from '@/context/CartContext';
 
+function ProductCard({id, name, price, description, category, image}) {
+    const { cart, setCart } = useContext(CartContext);
+
+    const handlAddToCart = () => {
+        const productPayload = { id, name, price, image, description, category };
+
+        const itemExists = cart.find((item) => item.id === id);
+        if (itemExists) {
+            setCart(
+                cart.map((item) =>
+                    item.id === id ? { ...item, quantity: (item.quantity || 1) + 1 } : item
+            )
+        );} 
+        else {
+        setCart([...cart, { ...productPayload, quantity: 1 }]);}
+    }
+}
