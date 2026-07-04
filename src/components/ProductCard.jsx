fb3bd660f783466b72f39fb9c518a97632c2800a
Conrad008 <kipngeno7180@gmail.com>
2026-07-05 00:22:36 +0300

    create ProductCard.jsx file and add CartContext and Lucide/Shadcn UI imports

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,6 @@
+import { useContext } from 'react';
+import { ShoppingBag } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { CartContext } from '../context/cartContext';
+
