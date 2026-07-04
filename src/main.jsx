cb04aecffe6ea224e6e6d6a511cb048472a45a4b
Conrad008 <kipngeno7180@gmail.com>
2026-07-04 19:44:52 +0300

    feat:Wrap App function in Context PROVIDERS

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext'
+import { CartProvider } from './context/CartContext'
+import {AuthProvider} from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <CartProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </CartProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
