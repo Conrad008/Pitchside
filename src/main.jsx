4bb85b889540c6d26b70a7e27574c5f1ec8035fa
Conrad008 <kipngeno7180@gmail.com>
2026-07-05 21:41:23 +0300

    fix: fix document path

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '@/App.jsx'
-import { ThemeProvider } from './context/ThemeContext'
-import { CartProvider } from './context/CartContext'
-import {AuthProvider} from './context/AuthContext'
+import { ThemeProvider } from '@/context/ThemeContext'
+import { CartProvider } from '@/context/CartContext'
+import {AuthProvider} from '@/context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
