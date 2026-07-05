84e1a662906dbb71b1858b86089256b26d9b9ec9
Conrad008 <kipngeno7180@gmail.com>
2026-07-05 21:54:07 +0300

    fix: change import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Login} from '@/pages/Login'
 import { Register } from "@/pages/Register"
-import Navbar from "@/components/NavBar"
+import {Navbar} from "@/components/NavBar"
 import ProductsList from "@/components/ProductList"
 import Checkout from "@/components/Checkout"
 import { Home } from "@/pages/Home";
